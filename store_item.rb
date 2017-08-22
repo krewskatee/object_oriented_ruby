a399455a2c860fa1bb8649088e15230a532dec54
krewskatee <lchira99@gmail.com>
2017-08-21 20:48:45 -0500

    added store_item file

---
+++ store_item.rb
@@ -0,0 +1,3 @@
+chair = {color: green, height: 3, legs: 1, price: 30}
+computer = {:color => "black", :height => 30, :shape => "square", :price => 550}
+book = {"title" => "Harry Potter", "cover" => "hardcover", "price" => 10}
