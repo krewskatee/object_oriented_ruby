7b5deafb02983a64616322449df66bb76b07ec61
krewskatee <lchira99@gmail.com>
2017-08-22 20:31:33 -0500

    added more files about inheritance

--- employee.rb
+++ employee.rb
@@ -1,32 +1,84 @@
 employee_1 = {first_name: "Jimmy", last_name: "Stewart", salary: 70000, active: true}
 employee_2 = {first_name: "Susan", last_name: "Messing", salary: 80000, active: true}
 
-puts "#{employee_1[:first_name]} #{employee_1[:last_name]} makes $#{employee_1[:salary]} a year."
-puts "#{employee_2[:first_name]} #{employee_2[:last_name]} makes $#{employee_2[:salary]} a year."
+# puts "#{employee_1[:first_name]} #{employee_1[:last_name]} makes $#{employee_1[:salary]} a year."
+# puts "#{employee_2[:first_name]} #{employee_2[:last_name]} makes $#{employee_2[:salary]} a year."
+
 
 class Employee
-  def initialize(first_name, last_name, salary, active)
-    @first_name = first_name
-    @last_name = last_name
-    @salary = salary
-    @active = active
+  attr_reader :first_name, :last_name, :salary, :active
+  attr_writer :first_name, :last_name, :salary, :active
+
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
   end
 
-  def first_name
-    return @first_name
+  def give_annual_raise
+    @salary = @salary * 1.10
   end
 
   def info
     return "#{@first_name} #{@last_name} makes $#{@salary} a year."
   end
 
-  def give_annual_raise
-    @salary = @salary * 1.05
-  end
 end
 
-employee_1 = Employee.new("Robert", "Stewart", 90000, true)
+class Manager < Employee
+
+  def initialize(input_options)
+    super(input_options)
+    @employees = input_options[:employees]
+  end
+
+  def send_report
+    puts "Sending Email..."
+    #....
+    puts "Email Sent"
+  end
+
+  def give_all_raises
+    @employees.each do |i|
+      i.give_annual_raise
+    end
+  end
+
+  def fire_all_employees
+    @employees.each do |i|
+      i.active = false
+    end
+  end
+
+
+
+
+end
+
+
+employee_1 = Employee.new(
+                          first_name: "Robert",
+                          last_name: "Stewart",
+                          salary: 90000,
+                          active: true
+                          )
+
+manager_1 = Manager.new(
+                          first_name: "Luke",
+                          last_name: "Stewart",
+                          salary: 90000,
+                          active: true,
+                          employees: [employee_1]
+                          )
+
 
 p employee_1.info
-p employee_1.give_annual_raise
-p employee_1.info
+employee_1.first_name = "Luke"
+employee_1.salary = 1000000
+p manager_1
+manager_1.give_all_raises
+p manager_1
+p employee_1.salary
+manager_1.fire_all_employees
+p employee_1.active
--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,38 @@
-chair = {color: green, height: 3, legs: 1, price: 30}
-computer = {:color => "black", :height => 30, :shape => "square", :price => 550}
-book = {"title" => "Harry Potter", "cover" => "hardcover", "price" => 10}
+# item_1 = {name: "Chair", color: "green", price: 30}
+# item_2 = {:name => "HP", :color => "black", :price => 550}
+# item_3 = {"name" => "Harry Potter", "color" => "black", "price" => 10}
+
+class StoreItem
+  attr_reader :name, :color, :price
+  attr_writer :name, :color, :price
+
+  def initialize(input_options)
+    @name = input_options[:name]
+    @color = input_options[:color]
+    @price = input_options[:price]
+    @height = input_options[:height]
+  end
+end
+
+item_1 = StoreItem.new(
+                      name: "Chair",
+                      color: "green",
+                      height: 3,
+                      price: 30
+                      )
+
+item_2 = StoreItem.new(
+                      name: "HP",
+                      color: "black",
+                      price: 550
+                      )
+                      
+item_3 = StoreItem.new(
+                      name: "Harry Potter",
+                      color: "color",
+                      price: 10
+                      )
+
+p item_1.name
+item_1.name = "not_char"
+p item_1.name
